bdd7450551184f5740cc137d9545999875117fc1
Hector <hedomec@gmail.com>
2025-10-14 18:05:37 -0600

    feat: update box selection handling to support multiple selections and improve category validation

--- src/app/modules/game/ui/main/game.ts
+++ src/app/modules/game/ui/main/game.ts
@@ -17,33 +17,35 @@ export class Game {
   _categoriesService = inject(CategoriesService);
   _productsService = inject(ProductsService);
 
-  box?: Box;
+  boxesSelected: Box[] = [];
   category?: Category;
 
   categories: Category[] = [];
   products: Product[] = [];
+
+
 
   async ngOnInit() {
     await this.loadCategories();
   }
 
   boxSelected(event: any) {
-    this.box = event;
-    console.log(this.box);
+    this.boxesSelected?.push(event);
+    console.log(this.boxesSelected);
   }
 
   catSelect(event: any) {
     this.category = event;
     console.log(this.category);
 
-    if (!this.box) {
+    if (this.boxesSelected?.length <= 0) {
       console.log('Select a box first');
       return;
     }
 
     if (this.category && this.category.categoryId === this.box?.categoryId) {
       console.log('Correct match!');
-      this.box = undefined;
+      this.boxesSelected = [];
       this.category = undefined;
     }
   }
